f9b574b042ff73d044584f3381acc06f817a86d3
Eyal <54035934+zeczen@users.noreply.github.com>
2022-12-21 18:01:33 +0200

    Update qrcode_generator.py
    Generate QRs according to the nfc ids in the database

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -1,45 +1,40 @@
-# pip install prcode
 import qrcode
-from base64 import b64encode
-from random import randrange
-import argparse
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+from supabase import create_client, Client 
 
- # Create the parser
-parser = argparse.ArgumentParser(prog='qrcode_generator.py', description='QRCode Generator')
+URL = "https://lifxuidjisbluwwautjd.supabase.co"
+KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpZnh1aWRqaXNibHV3d2F1dGpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE1NDUwNDksImV4cCI6MTk4NzEyMTA0OX0.u0vX3jzEiFoFSoCMGUoPj-oloJAOvUXo_wUEFMB3Zl0"
+W = 6 
+H = 4
+font_path = "/usr/share/fonts/truetype/fonts-yrsa-rasa/Yrsa-SemiBold.ttf"  # for unix
+# font_path = "arial.ttf"  # should work for windows
 
-# Add the arguments
-parser.add_argument('-n', '--number_of_qrs', action='store', help='How much QRs you want to generate?', required=True, type=int)
+font = ImageFont.truetype(font_path, 48)
+text_color = 0
 
-args = parser.parse_args()
-
-W = 7
-H = 5
-
-def generate_id():
-    return b64encode(str(randrange(10 ** 5)).encode())
-
-def create_image(n, j):
+def create_image(ids):
     size = 290
     page = Image.new('RGB', (W * size + size, H * size + size))
 
-    for i in range(n):
-        qr_code = qrcode.make(data=generate_id()).get_image()
+    for i in range(len(ids)):
+        qr_code = qrcode.make(data=ids[i]).get_image()
+        ImageDraw.Draw(qr_code).text((50, 0), ids[i], font=font, fill=text_color)        
         x = int(size * ((i % W) * (1 + 1/(W-1))))
         y = int(size * ((i // W) * (1 + 1/(W-1))))
         page.paste(qr_code, (x, y))
     return page
 
 def main():
-    n = int(args.number_of_qrs)
-    i = 0
+    supabase = create_client(URL, KEY)
+    ids = [i['id'] for i in supabase.table("patients").select("id").execute().data]
+
     images = []
-    while n > 0:
-        current_image = min(H * W, n)
-        images.append(create_image(current_image, i))
-        n -= current_image
-        i += 1
-    images[0].save('qrcodes.pdf', "PDF" ,resolution=100.0, save_all=True, append_images=images)
+    while ids:
+        n = min(H * W, len(ids))
+        images.append(create_image(ids[:n]))
+        ids = ids[n:] 
+    images[0].save('qrcodes.pdf', "PDF" ,resolution=100.0, save_all=True, append_images=images[1:])
+
 
 if __name__ == '__main__':
     main()
